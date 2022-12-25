f1e1b0af7b6401bdc740c674864ba97927b1413e
Gaurav Singh <gtx25801@gmail.com>
2022-12-25 16:19:57 +0530

    task 12

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 let form = document.querySelector("#user-details");
-
+let users = document.querySelector("#items");
 
 form.addEventListener("submit",storeDetails);
 
@@ -18,7 +18,29 @@ function storeDetails(e){
         useremail : email.value
     }
     newObj_serialized = JSON.stringify(newObj);
+    let key = Math.floor((1 + Math.random()) * 0x10000).toString(16).substring(1);
 
-    localStorage.setItem("myObj" , newObj_serialized );
-    
+    localStorage.setItem(key , newObj_serialized );
+
+}
+
+function refreshUserList(){
+    for (let i = 0; i < localStorage.length; i++) {
+        let userAdded = false;
+        const user = JSON.parse(localStorage.getItem(localStorage.key(i)));
+        let li = document.createElement("li");
+        li.className = "list-group-item";
+        let text = `${user.username} || ${user.useremail}`;
+        let addedUsers = document.querySelectorAll(".list-group-item");
+        addedUsers.forEach( addedUser => {
+            if (addedUser.textContent === text){
+                userAdded = true;
+            }
+        });
+        if (!userAdded){
+            
+        li.appendChild(document.createTextNode(text));
+        users.appendChild(li);
+        }
+    }
 }
